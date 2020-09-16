ac56c8b2721602f826667ea090ecbaa80a719907
Zaida Rivai <zaidarivai@MacBook-Pro-van-Zaida-2.local>
2020-09-16 17:56:46 +0200

    added markdown

---
+++ isthereacorrealation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 16 17:38:24 2020
+
+@author: zaidarivai
+"""
+
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+df = pd.read_csv("istherecorrelation.csv",delimiter=';')
+
+fig, ax = plt.subplots()
+
+# fig.set_size_inches(3, 1.5)
+
+
+plt.scatter(df.iloc[:,0],df.iloc[:,-1],label="Netherlands")
+# plt.scatter(df.iloc[:,0],df.iloc[:,1])
+plt.xlabel("WO (x1000)")
+plt.ylabel("Annually beer consumption (x1000 hectoliter)")
+plt.legend()
+plt.show()
+plt.savefig('myfig.jpg')
--- solution_zaida_rivai.md
+++ solution_zaida_rivai.md
@@ -1,3 +1,10 @@
 # MCC Van Dyke et al., 2019
 # JT Harvey, Applied Ergonomics, 2002
 # DW Ziegler et al., 2005
+
+Here's an image of a drag racer in action:
+
+![Drag Racing](myfig.png)
+
+
+
